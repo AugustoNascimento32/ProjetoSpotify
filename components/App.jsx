a3b9dbd4148b661a2be6f51bbd223c1ae915bde9
GutoNasc32 <augusto.nasc.05@gmail.com>
2025-02-13 21:20:48 -0300

    aula1 SpotifyProject

---
+++ components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../Header";
+import Main from "../Mainn";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+};
+
+export default App;
---
+++ components/App2.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const App2 = () => {
+  return <div>App2</div>;
+};
+
+export default App2;
+
+//rafce
+
+//Arrow Function
+// const App2 = () => <h1>Olá, Mundo!</h1>;
+
+//nomeação de componentes
+//PascalCase
+
+// export default, posso importar com qq nome e sem chaves
+//export sem default só posso importar com chaves e com o nome exportado
